eb5927a129f870e4c4d26417b2405b04879aab48
Jet Kingston <jettaikingston10@gmail.com>
2024-04-29 20:13:49 +0200

    Add project files.

---
+++ area of a triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace area_of_a_triangle
+{
+    internal class Program
+    {
+        static double area; 
+        static void Main(string[] args)
+        {
+            //Declarations 
+            double Height;
+            double Base;
+
+            Console.WriteLine("what is the height of the triangle");
+            Height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("what is the base of the triangle");
+            Base = int.Parse(Console.ReadLine());
+
+            // call method 
+            CalculateArea(Height, Base);
+
+        }
+        static void CalculateArea(double Height, double Base)
+        {
+           
+             double area = 0.5 * (Height * Base);
+
+
+            Console.WriteLine("the area of the triangle is:" + area);
+            Console.ReadLine();
+
+            return ;
+         
+        }
+       
+    }
+}
